b4aad07d4027c95a80af81bea6e2a8a98ef1a375
Tsai <a03247247@gmail.com>
2021-05-07 01:32:41 +0800

    算出平均長度

--- read.py
+++ read.py
@@ -8,3 +8,10 @@ with open('reviews.txt', 'r') as f:
         if count %1000 == 0:
             print(len(data))
 
+print('檔案讀取結束,總共有', len(data),"筆資料")
+
+sum_len = 0
+for d in data:
+    sum_len += len(d)  #sum_lem = sum_len + len(d)
+    
+print ('平均長度為', sum_len / len(data))
