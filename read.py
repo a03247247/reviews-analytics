92c6286bef20dacd582d28aa018c3f6bbe16247c
Tsai <a03247247@gmail.com>
2021-05-19 19:49:12 +0800

    新增記數功能，新增搜尋字典功能

--- read.py
+++ read.py
@@ -5,8 +5,8 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count %10000 == 0:
-         print(len(data))
+        if count %100000 == 0:
+            print(len(data))
 
 print('檔案讀取結束,總共有', len(data),"筆資料")
 
@@ -21,4 +21,33 @@ for d in data:
     if len(d) < 100:
         new.append(d)
 print('一共有' ,len(new),'筆資料長度小於100')
-print(new[0])
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('共有', len(good), '比留言提到good')
+
+wc = {}  #word_count
+for d in data:
+    words = d.split(' ')
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1
+
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+
+while True:
+    word = input('您想搜尋的字:')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, '出現過的次數為: ', wc[word])
+    else:
+        print('這個字沒有出現過喔!')
+
+print('感謝使用本功能')
